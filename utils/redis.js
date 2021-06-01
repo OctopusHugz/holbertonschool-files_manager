59c2be6e82ae62a3dc58cfb8eedf22a195d82202
OctopusHugz <colsonscott53@gmail.com>
2021-06-01 22:34:44 +0000

    Added utils/redis.js

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
+  }
+
+  isAlive() {
+    // This doesn't wait until connection is established
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key).catch(console.error);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    await asyncSet(key, value, 'EX', duration).catch(console.error);
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key).catch(console.error);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
