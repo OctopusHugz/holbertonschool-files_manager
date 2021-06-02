262396c93dde01561b6dd6776c3b58a4fd0dc00d
OctopusHugz <colsonscott53@gmail.com>
2021-06-02 03:54:08 +0000

    Added app.use(express.json()) to parse request body

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const indexRouter = require('./routes/index');
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server running on port ${port}`));
+app.use(express.json());
 app.use('/', indexRouter);
 
 module.exports = app;
