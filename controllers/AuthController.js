e17def557fe49fb15182861b1b7f34bc8bb78121
OctopusHugz <colsonscott53@gmail.com>
2021-06-02 15:40:05 +0000

    Added getConnect method

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,21 +1,27 @@
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
+
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AuthController {
-  static getConnect(request, response) {
-    // By using the header Authorization and the technique
-    // of the Basic auth(Base64 of the < email >: <password>),
-    // find the user associate to this email and with this password
-    // (reminder: we are storing the SHA1 of the password)
+  static async getConnect(request, response) {
+    const fullAuthHeader = request.headers.authorization;
+    const b64AuthHeader = fullAuthHeader.slice(6);
+    const userCreds = Buffer.from(b64AuthHeader, 'base64').toString();
+    const email = userCreds.split(':')[0];
+    const password = userCreds.split(':')[1];
+    const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
+    const users = dbClient.db.collection('users');
+    const userExistsArray = await users.find({ email, password: hashedPassword }).toArray();
 
-    // If no user has been found, return an error Unauthorized with a status code 401
+    if (userExistsArray.length === 0) response.status(401).json({ error: 'Unauthorized' });
 
-    // Otherwise:
-    // Generate a random string (using uuidv4) as token
-    // Create a key: auth_<token>
-    // Use this key for storing in Redis
-    // (by using the redisClient create previously) the user ID for 24 hours
-    // Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" } with a status code 200
+    const user = userExistsArray[0];
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+    response.json({ token });
   }
 
   static getDisconnect(request, response) {
