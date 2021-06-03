d6cca4ea2c95587ddec8b22a99f33c650a761f13
OctopusHugz <colsonscott53@gmail.com>
2021-06-03 23:01:16 +0000

    Added return before all response.json calls

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -11,18 +11,18 @@ class UsersController {
 
     if (!email) {
       response.statusCode = 400;
-      response.json({ error: 'Missing email' });
+      return response.json({ error: 'Missing email' });
     }
     if (!password) {
       response.statusCode = 400;
-      response.json({ error: 'Missing password' });
+      return response.json({ error: 'Missing password' });
     }
 
     const users = dbClient.db.collection('users');
     const userExistsArray = await users.find({ email }).toArray();
     if (userExistsArray.length > 0) {
       response.statusCode = 400;
-      response.json({ error: 'Already exist' });
+      return response.json({ error: 'Already exist' });
     }
 
     const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
@@ -30,19 +30,19 @@ class UsersController {
     const createdUser = { id: resultObj.ops[0]._id, email: resultObj.ops[0].email };
     userQueue.add({ userId: createdUser.id });
     response.statusCode = 201;
-    response.json(createdUser);
+    return response.json(createdUser);
   }
 
   static async getMe(request, response) {
     const token = request.headers['x-token'];
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
-    if (userId === null) response.status(401).json({ error: 'Unauthorized' });
+    if (userId === null) return response.status(401).json({ error: 'Unauthorized' });
 
     const users = dbClient.db.collection('users');
     const userExistsArray = await users.find(`ObjectId("${userId}")`).toArray();
     const user = userExistsArray[0];
-    response.json({ id: user._id, email: user.email });
+    return response.json({ id: user._id, email: user.email });
   }
 }
 
