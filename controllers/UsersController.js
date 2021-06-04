ff111a63388c82835b4f27a047fba957321f62d3
OctopusHugz <colsonscott53@gmail.com>
2021-06-04 05:43:22 +0000

    Added helper func to getMe

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,14 +1,16 @@
 const crypto = require('crypto');
 const Queue = require('bull');
 const dbClient = require('../utils/db');
-const { checkAuth } = require('../utils/helpers');
+const { checkAuth, findUserById } = require('../utils/helpers');
 
 class UsersController {
   static async postNew(request, response) {
     const userQueue = new Queue('userQueue');
     const { email } = request.body;
     const { password } = request.body;
+    const users = dbClient.db.collection('users');
 
+    // Refactor checks for missing attributes into inputValidation()
     if (!email) {
       response.statusCode = 400;
       return response.json({ error: 'Missing email' });
@@ -18,7 +20,6 @@ class UsersController {
       return response.json({ error: 'Missing password' });
     }
 
-    const users = dbClient.db.collection('users');
     const userExistsArray = await users.find({ email }).toArray();
     if (userExistsArray.length > 0) {
       response.statusCode = 400;
@@ -35,9 +36,7 @@ class UsersController {
 
   static async getMe(request, response) {
     const userId = await checkAuth(request, response);
-    const users = dbClient.db.collection('users');
-    const userExistsArray = await users.find(`ObjectId("${userId}")`).toArray();
-    const user = userExistsArray[0];
+    const user = await findUserById(userId);
     return response.json({ id: user._id, email: user.email });
   }
 }
