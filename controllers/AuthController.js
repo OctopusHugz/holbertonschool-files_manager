3c0a60c797cae2eba6e433cc4b69425a7ac6a61c
OctopusHugz <colsonscott53@gmail.com>
2021-06-04 05:42:13 +0000

    Refactored into helpers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,36 +1,21 @@
-const crypto = require('crypto');
 const { v4: uuidv4 } = require('uuid');
-
 const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+const { checkAuthReturnKey, findUserByCreds, credsFromBasicAuth } = require('../utils/helpers');
 
 class AuthController {
   static async getConnect(request, response) {
-    const fullAuthHeader = request.headers.authorization;
-    const b64AuthHeader = fullAuthHeader.slice(6);
-    const userCreds = Buffer.from(b64AuthHeader, 'base64').toString();
-    const email = userCreds.split(':')[0];
-    const password = userCreds.split(':')[1];
-    const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
-    const users = dbClient.db.collection('users');
-    const userExistsArray = await users.find({ email, password: hashedPassword }).toArray();
-
-    if (userExistsArray.length === 0) response.status(401).json({ error: 'Unauthorized' });
-
-    const user = userExistsArray[0];
+    const creds = await credsFromBasicAuth(request);
+    const user = await findUserByCreds(response, creds.email, creds.password);
     const token = uuidv4();
     const key = `auth_${token}`;
     await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
-    response.json({ token });
+    return response.json({ token });
   }
 
   static async getDisconnect(request, response) {
-    const token = request.headers['x-token'];
-    const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
-    if (userId === null) response.status(401).json({ error: 'Unauthorized' });
+    const key = await checkAuthReturnKey(request, response);
     await redisClient.del(key);
-    response.status(204).end();
+    return response.status(204).end();
   }
 }
 
