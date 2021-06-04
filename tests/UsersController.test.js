79ed55f98da5e7bea6cc128952027498abdb417d
OctopusHugz <colsonscott53@gmail.com>
2021-06-04 17:55:04 +0000

    Added tests for .getMe()

--- tests/UsersController.test.js
+++ tests/UsersController.test.js
@@ -3,22 +3,14 @@ const { ObjectID } = require('mongodb');
 const { expect } = require('chai');
 const request = require('request');
 const dbClient = require('../utils/db');
-
-const { getRandomInt } = require('../utils/helpers');
+const redisClient = require('../utils/redis');
+const { getRandomInt, credsFromAuthHeaderString, findUserByCreds } = require('../utils/helpers');
 
 const randomUserId = getRandomInt(1, 99999999);
 const randomPassword = getRandomInt(1, 99999999);
 
 describe('UsersController', () => {
   it('checks the return of postNew with a random new user', (done) => {
-    // Below code is possible fix to timeout of dbClient,
-    // but search for more robust fix
-
-    // const dbConnected = dbClient.isAlive();
-    // while (!dbConnected) {
-    //   console.log('Trying to reconnect to dbClient!');
-    //   dbConnected = dbClient.isAlive();
-    // }
     const users = dbClient.db.collection('users');
     const bodyData = {
       email: `testuser${randomUserId}@email.com`,
@@ -93,6 +85,77 @@ describe('UsersController', () => {
       const jBody = JSON.parse(body);
       expect(response.statusCode).to.equal(400);
       expect(jBody).to.deep.equal({ error: 'Missing password' });
+      done();
+    });
+  });
+
+  // rewrite using fetch() to avoid callback hell?
+  it('checks the return of .getMe() with valid user', (done) => {
+    // Authorization: Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=
+    // for user 'bob@dylan.com'
+    let token;
+    const headerData = {
+      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
+    };
+    request({
+      url: 'http://0.0.0.0:5000/connect',
+      headers: headerData,
+    }, async (error, response, body) => {
+      const jBody = JSON.parse(body);
+      const creds = await credsFromAuthHeaderString(headerData.Authorization);
+      const user = await findUserByCreds(response, creds.email, creds.password);
+      token = jBody.token;
+      expect(response.statusCode).to.equal(200);
+      expect(jBody).to.have.property('token');
+      expect(token.length).to.equal(36);
+      expect(await redisClient.get(`auth_${token}`)).to.equal(user._id.toString());
+
+      // GET /users/me with header:
+      // X-Token: ${tokenFromGetConnect}
+      const tokenHeader = { 'X-Token': token };
+      request({
+        url: 'http://0.0.0.0:5000/users/me',
+        headers: tokenHeader,
+      }, async (error, response, body) => {
+        const jBody = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        expect(jBody).to.have.property('id');
+        expect(jBody).to.have.property('email');
+        expect(jBody.id.length).to.equal(24);
+        expect(await redisClient.get(`auth_${token}`)).to.equal(jBody.id);
+        expect(jBody.email).to.equal('bob@dylan.com');
+
+        request({
+          url: 'http://0.0.0.0:5000/disconnect',
+          headers: tokenHeader,
+        }, async (error, response, body) => {
+          expect(response.statusCode).to.equal(204);
+          expect(body).to.equal('');
+          expect(await redisClient.get(`auth_${token}`)).to.equal(null);
+
+          request({
+            url: 'http://0.0.0.0:5000/users/me',
+            headers: tokenHeader,
+          }, async (error, response, body) => {
+            const jBody = JSON.parse(body);
+            expect(response.statusCode).to.equal(401);
+            expect(jBody).to.deep.equal({ error: 'Unauthorized' });
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('checks the return of .getMe() with invalid user', (done) => {
+    const headerData = { 'X-Token': '031bffac-3edc-4e51-aaae-1c121317da8a' };
+    request({
+      url: 'http://0.0.0.0:5000/users/me',
+      headers: headerData,
+    }, async (error, response, body) => {
+      const jBody = JSON.parse(body);
+      expect(response.statusCode).to.equal(401);
+      expect(jBody).to.deep.equal({ error: 'Unauthorized' });
       done();
     });
   });
