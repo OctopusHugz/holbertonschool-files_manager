2f0ce41056c92c3e513990976bcbbb0f8786d63e
OctopusHugz <colsonscott53@gmail.com>
2021-06-06 06:15:02 +0000

    Removed limit from express.json

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', indexRouter);
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.listen(port, console.log(`Server running on port ${port}`));
 
 export default app;
