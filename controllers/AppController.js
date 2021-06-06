683527e4d6e70beeb2ef89a9cb106728ce3d46e7
OctopusHugz <colsonscott53@gmail.com>
2021-06-06 06:09:47 +0000

    Changed response.json to response.send

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,12 +4,12 @@ import dbClient from '../utils/db';
 class AppController {
   static getStatus(request, response) {
     const resObj = { redis: redisClient.isAlive(), db: dbClient.isAlive() };
-    return response.json(resObj);
+    response.send(resObj);
   }
 
   static async getStats(request, response) {
     const resObj = { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() };
-    return response.json(resObj);
+    response.send(resObj);
   }
 }
 
