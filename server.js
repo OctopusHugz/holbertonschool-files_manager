2b50e96a4f8fbbc489eea763cbebba55586e7274
OctopusHugz <colsonscott53@gmail.com>
2021-06-06 07:21:59 +0000

    Moved express.json() before app.use

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use('/', indexRouter);
 app.use(express.json());
+app.use('/', indexRouter);
 app.listen(port, console.log(`Server running on port ${port}`));
 
 export default app;
