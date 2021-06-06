8be8549680f1dff7df07fd77cca2d001b42fe23a
OctopusHugz <colsonscott53@gmail.com>
2021-06-06 20:19:22 +0000

    Fixed getFile returning 404 if user wasn't found, can stil return 200

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -107,10 +107,13 @@ class FilesController {
 
   static async getFile(request, response) {
     const token = request.headers['x-token'];
-    const userId = await getFileCheckAuth(request, response);
+    const userId = await getFileCheckAuth(request);
     const files = dbClient.db.collection('files');
     const { size } = request.query;
     const file = await findFile(request, response, files, userId);
+    // console.log('file :>> ', file);
+    // make getFileCheckAuth return null first
+    // if (userId === null && )
     return checkFileAndReadContents(response, file, token, userId, size);
   }
 }
