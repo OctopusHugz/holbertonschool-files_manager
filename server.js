04220930638111c57f91d8aa6ee75362d7b36a3b
OctopusHugz <colsonscott53@gmail.com>
2021-06-06 05:58:29 +0000

    Moved app.listen below app.use

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const indexRouter = require('./routes/index');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server running on port ${port}`));
-app.use(express.json({ limit: '50mb' }));
 app.use('/', indexRouter);
+app.use(express.json({ limit: '50mb' }));
+app.listen(port, console.log(`Server running on port ${port}`));
 
 module.exports = app;
