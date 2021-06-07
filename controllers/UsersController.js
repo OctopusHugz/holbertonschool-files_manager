775316aad8e43d000ca4f86426772acdb965136a
OctopusHugz <colsonscott53@gmail.com>
2021-06-07 18:56:11 +0000

    Moved error handling into controller from helper

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,23 +1,28 @@
+import crypto from 'crypto';
 import Queue from 'bull';
 import dbClient from '../utils/db';
-import { checkAuth, findUserById, userInputValidation } from '../utils/helpers';
+import { checkAuth, findUserById } from '../utils/helpers';
 
 class UsersController {
   static async postNew(request, response) {
     const userQueue = new Queue('userQueue');
-    const { email } = request.body;
-    const { password } = request.body;
-    const hashedPassword = await userInputValidation(response, email, password);
+    const { email, password } = request.body;
+    if (!email) return response.status(400).json({ error: 'Missing email' });
+    if (!password) return response.status(400).json({ error: 'Missing password' });
 
+    const userExistsArray = await dbClient.users.find({ email }).toArray();
+    if (userExistsArray.length > 0) return response.status(400).json({ error: 'Already exist' });
+
+    const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
     const resultObj = await dbClient.users.insertOne({ email, password: hashedPassword });
     const createdUser = { id: resultObj.ops[0]._id, email: resultObj.ops[0].email };
     userQueue.add({ userId: createdUser.id });
-    response.statusCode = 201;
-    return response.json(createdUser);
+    return response.status(201).json(createdUser);
   }
 
   static async getMe(request, response) {
-    const userId = await checkAuth(request, response);
+    const userId = await checkAuth(request);
+    if (!userId) return response.status(401).json({ error: 'Unauthorized' });
     const user = await findUserById(userId);
     return response.json({ id: user._id, email: user.email });
   }
