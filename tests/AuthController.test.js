3b6075fa94305b290b0b92e0476fbe6bf46a7d6b
OctopusHugz <colsonscott53@gmail.com>
2021-06-07 17:38:30 +0000

    Refactored using chai.request and async/await

--- tests/AuthController.test.js
+++ tests/AuthController.test.js
@@ -1,16 +1,19 @@
-import { expect } from 'chai';
-import request from 'request';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 import { findUserByCreds, credsFromAuthHeaderString } from '../utils/helpers';
 
-let token;
+chai.use(chaiHttp);
 
 describe('AuthController', () => {
   beforeEach(async () => {
     await dbClient.users.deleteMany({});
     await dbClient.files.deleteMany({});
     await dbClient.users.insertOne({ email: 'bob@dylan.com', password: '89cad29e3ebc1035b29b1478a8e70854f25fa2b2' });
+    // Authorization: Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=
+    // for user 'bob@dylan.com'
   });
 
   afterEach(async () => {
@@ -18,66 +21,54 @@ describe('AuthController', () => {
     await dbClient.files.deleteMany({});
   });
 
-  it('checks the return of .getConnect() with valid user', (done) => {
-    // Authorization: Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=
-    // for user 'bob@dylan.com'
+  it('GET /connect with valid user', async () => {
     const headerData = {
       Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
     };
-    request({
-      url: 'http://0.0.0.0:5000/connect',
-      headers: headerData,
-    }, async (error, response, body) => {
-      const jBody = JSON.parse(body);
-      const creds = await credsFromAuthHeaderString(headerData.Authorization);
-      const user = await findUserByCreds(response, creds.email, creds.password);
-      token = jBody.token;
-      expect(response.statusCode).to.equal(200);
-      expect(jBody).to.have.property('token');
-      expect(token.length).to.equal(36);
-      expect(await redisClient.get(`auth_${token}`)).to.equal(user._id.toString());
-      done();
-    });
+    const res = await chai.request(app).get('/connect').set(headerData).catch(console.error);
+    const creds = await credsFromAuthHeaderString(headerData.Authorization);
+    const user = await findUserByCreds(creds.email, creds.password);
+    const { token } = res.body;
+    expect(res).to.have.status(200);
+    expect(res.body).to.have.property('token');
+    expect(token.length).to.equal(36);
+    expect(await redisClient.get(`auth_${token}`)).to.equal(user._id.toString());
   });
 
-  it('checks the return of .getConnect() with invalid user', (done) => {
+  it('GET /connect with invalid user', async () => {
     const headerData = {
       Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIchop=',
     };
-    request({
-      url: 'http://0.0.0.0:5000/connect',
-      headers: headerData,
-    }, async (error, response, body) => {
-      const jBody = JSON.parse(body);
-      expect(response.statusCode).to.equal(401);
-      expect(jBody).to.deep.equal({ error: 'Unauthorized' });
-      done();
-    });
+    const res = await chai.request(app).get('/connect').set(headerData).catch(console.error);
+    expect(res).to.have.status(401);
+    expect(res.body).to.deep.equal({ error: 'Unauthorized' });
   });
 
-  it('checks the return of .getDisconnect() with valid user', (done) => {
+  it('GET /connect with invalid auth', async () => {
+    const headerData = {
+      Authorization: 'hello friend',
+    };
+    const res = await chai.request(app).get('/connect').set(headerData).catch(console.error);
+    expect(res).to.have.status(401);
+    expect(res.body).to.deep.equal({ error: 'Unauthorized' });
+  });
+
+  it('GET /disconnect with valid user', async () => {
+    const authHeaderData = {
+      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
+    };
+    const connectRes = await chai.request(app).get('/connect').set(authHeaderData).catch(console.error);
+    const { token } = connectRes.body;
     const headerData = { 'X-Token': token };
-    request({
-      url: 'http://0.0.0.0:5000/disconnect',
-      headers: headerData,
-    }, async (error, response, body) => {
-      expect(response.statusCode).to.equal(204);
-      expect(body).to.equal('');
-      expect(await redisClient.get(`auth_${token}`)).to.equal(null);
-      done();
-    });
+    const disconnectRes = await chai.request(app).get('/disconnect').set(headerData).catch(console.error);
+    expect(disconnectRes).to.have.status(204);
+    expect(await redisClient.get(`auth_${token}`)).to.equal(null);
   });
 
-  it('checks the return of .getDisconnect() with invalid user', (done) => {
+  it('GET /disconnect with invalid user', async () => {
     const headerData = { 'X-Token': '031bffac-3edc-4e51-aaae-1c121317da8a' };
-    request({
-      url: 'http://0.0.0.0:5000/disconnect',
-      headers: headerData,
-    }, async (error, response, body) => {
-      const jBody = JSON.parse(body);
-      expect(response.statusCode).to.equal(401);
-      expect(jBody).to.deep.equal({ error: 'Unauthorized' });
-      done();
-    });
+    const res = await chai.request(app).get('/disconnect').set(headerData).catch(console.error);
+    expect(res).to.have.status(401);
+    expect(res.body).to.deep.equal({ error: 'Unauthorized' });
   });
 });
