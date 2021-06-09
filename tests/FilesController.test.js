f1cf05bdcb0f39e994b1edaeee0b6f205c45f7e9
OctopusHugz <colsonscott53@gmail.com>
2021-06-09 04:22:36 +0000

    Refactoring some tests using GET /connect in beforeEach

--- tests/FilesController.test.js
+++ tests/FilesController.test.js
@@ -7,8 +7,11 @@ import app from '../server';
 
 chai.use(chaiHttp);
 const fPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+const headerData = { Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=' };
 let insertedFileId;
 let insertedUserId;
+let token;
+let postHeaders;
 
 describe('FilesController', () => {
   beforeEach(async () => {
@@ -23,6 +26,17 @@ describe('FilesController', () => {
     });
     insertedFileId = resultObj.ops[0]._id.toString();
     insertedUserId = userObj.ops[0]._id.toString();
+    return new Promise((resolve) => {
+      chai.request(app)
+        .get('/connect')
+        .set(headerData)
+        .then((res) => {
+          token = res.body.token;
+          postHeaders = { 'X-Token': token };
+          expect(res).to.have.status(200);
+          resolve();
+        });
+    });
   });
 
   afterEach(async () => {
@@ -31,119 +45,74 @@ describe('FilesController', () => {
   });
 
   it('POST /files with valid user', (done) => {
-    const headerData = {
-      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
-    };
+    const bodyData = { name: 'myText.txt', type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
     chai.request(app)
-      .get('/connect')
-      .set(headerData)
+      .post('/files')
+      .set(postHeaders)
+      .send(bodyData)
       .then(async (res) => {
         const creds = await credsFromAuthHeaderString(headerData.Authorization);
         const user = await findUserByCreds(creds.email, creds.password);
-        const { token } = res.body;
-        expect(res).to.have.status(200);
-        const postHeaders = { 'X-Token': token, 'Content-Type': 'application/json' };
-        const bodyData = { name: 'myText.txt', type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
-        chai.request(app)
-          .post('/files')
-          .set(postHeaders)
-          .send(bodyData)
-          .then(async (res) => {
-            const files = dbClient.db.collection('files');
-            const fileArray = await files.find(
-              { userId: ObjectID(user._id), _id: ObjectID(res.body.id) },
-            ).toArray();
-            const file = fileArray[0];
-            expect(res).to.have.status(201);
-            expect(res.body.id.length).to.equal(24);
-            expect(res.body.userId).to.equal(user._id.toString());
-            expect(res.body.name).to.equal('myText.txt');
-            expect(res.body.type).to.equal('file');
-            expect(res.body.isPublic).to.equal(false);
-            expect(res.body.parentId).to.equal(0);
-            expect(fileArray.length).to.equal(1);
-            expect(file._id.toString()).to.equal(res.body.id);
-            expect(file.userId.toString()).to.equal(user._id.toString());
-            expect(file.name).to.equal('myText.txt');
-            expect(file.type).to.equal('file');
-            expect(file.isPublic).to.equal(false);
-            expect(file.parentId).to.equal(0);
-            expect(file).to.have.property('localPath');
-            expect(file.localPath).to.contain(fPath);
-            done();
-          });
+        const fileArray = await dbClient.files.find(
+          { userId: ObjectID(user._id), _id: ObjectID(res.body.id) },
+        ).toArray();
+        const file = fileArray[0];
+        expect(res).to.have.status(201);
+        expect(res.body.id.length).to.equal(24);
+        expect(res.body.userId).to.equal(user._id.toString());
+        expect(res.body.name).to.equal('myText.txt');
+        expect(res.body.type).to.equal('file');
+        expect(res.body.isPublic).to.equal(false);
+        expect(res.body.parentId).to.equal(0);
+        expect(fileArray.length).to.equal(1);
+        expect(file._id.toString()).to.equal(res.body.id);
+        expect(file.userId.toString()).to.equal(user._id.toString());
+        expect(file.name).to.equal('myText.txt');
+        expect(file.type).to.equal('file');
+        expect(file.isPublic).to.equal(false);
+        expect(file.parentId).to.equal(0);
+        expect(file).to.have.property('localPath');
+        expect(file.localPath).to.contain(fPath);
+        done();
       });
   });
 
   it('POST /files with valid user, missing name in request body', (done) => {
-    const headerData = {
-      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
-    };
+    const bodyData = { type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
     chai.request(app)
-      .get('/connect')
-      .set(headerData)
-      .then(async (res) => {
-        const { token } = res.body;
-        expect(res).to.have.status(200);
-        const postHeaders = { 'X-Token': token, 'Content-Type': 'application/json' };
-        const bodyData = { type: 'file', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
-        chai.request(app)
-          .post('/files')
-          .set(postHeaders)
-          .send(bodyData)
-          .then((res) => {
-            expect(res).to.have.status(400);
-            expect(res.body).to.deep.equal({ error: 'Missing name' });
-            done();
-          });
+      .post('/files')
+      .set(postHeaders)
+      .send(bodyData)
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.deep.equal({ error: 'Missing name' });
+        done();
       });
   });
 
   it('POST /files with valid user, missing type in request body', (done) => {
-    const headerData = {
-      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
-    };
+    const bodyData = { name: 'myText.txt', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
     chai.request(app)
-      .get('/connect')
-      .set(headerData)
-      .then(async (res) => {
-        const { token } = res.body;
-        expect(res).to.have.status(200);
-        const postHeaders = { 'X-Token': token, 'Content-Type': 'application/json' };
-        const bodyData = { name: 'myText.txt', data: 'SGVsbG8gV2Vic3RhY2shCg==' };
-        chai.request(app)
-          .post('/files')
-          .set(postHeaders)
-          .send(bodyData)
-          .then((res) => {
-            expect(res).to.have.status(400);
-            expect(res.body).to.deep.equal({ error: 'Missing type' });
-            done();
-          });
+      .post('/files')
+      .set(postHeaders)
+      .send(bodyData)
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.deep.equal({ error: 'Missing type' });
+        done();
       });
   });
 
   it('POST /files with valid user, missing data in request body and type != folder', (done) => {
-    const headerData = {
-      Authorization: 'Basic Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=',
-    };
+    const bodyData = { name: 'myText.txt', type: 'file' };
     chai.request(app)
-      .get('/connect')
-      .set(headerData)
-      .then(async (res) => {
-        const { token } = res.body;
-        expect(res).to.have.status(200);
-        const postHeaders = { 'X-Token': token, 'Content-Type': 'application/json' };
-        const bodyData = { name: 'myText.txt', type: 'file' };
-        chai.request(app)
-          .post('/files')
-          .set(postHeaders)
-          .send(bodyData)
-          .then((res) => {
-            expect(res).to.have.status(400);
-            expect(res.body).to.deep.equal({ error: 'Missing data' });
-            done();
-          });
+      .post('/files')
+      .set(postHeaders)
+      .send(bodyData)
+      .then((res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.deep.equal({ error: 'Missing data' });
+        done();
       });
   });
 
